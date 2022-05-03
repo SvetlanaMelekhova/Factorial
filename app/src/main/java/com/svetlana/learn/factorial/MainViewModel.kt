919a4eea978bd5c2d6dda4a4c1a22b00ae505ce8
SvetlanaMelekhova <s.melekhova@dinamicadigital.io>
2022-05-03 20:38:20 +0300

    add state via sealed class

--- app/src/main/java/com/svetlana/learn/factorial/MainViewModel.kt
+++ app/src/main/java/com/svetlana/learn/factorial/MainViewModel.kt
@@ -13,29 +13,17 @@ class MainViewModel : ViewModel() {
     val state: LiveData<State>
         get() = _state
 
-    private val _error = MutableLiveData<Boolean>()
-    val error: LiveData<Boolean>
-        get() = _error
-
-    private val _factorial = MutableLiveData<String>()
-    val factorial: LiveData<String>
-        get() = _factorial
-
-    private val _progress = MutableLiveData<Boolean>()
-    val progress: LiveData<Boolean>
-        get() = _progress
-
     fun calculate(value: String?) {
-        _state.value = State(isInProgress = true)
+        _state.value = Progress
         if (value.isNullOrBlank()) {
-            _state.value = State(isError = true)
+            _state.value = Error
             return
         }
         viewModelScope.launch {
             val number = value.toLong()
             //calculate
             delay(1000)
-            _state.value = State(factorial = number.toString())
+            _state.value = Result(number.toString())
         }
     }
 }
