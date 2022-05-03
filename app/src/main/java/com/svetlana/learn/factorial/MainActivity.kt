6792fd1b3496c9cc41742ae21350c5dd5a3a4afe
SvetlanaMelekhova <s.melekhova@dinamicadigital.io>
2022-05-03 15:05:50 +0300

    setup main layout

--- app/src/main/java/com/svetlana/learn/factorial/MainActivity.kt
+++ app/src/main/java/com/svetlana/learn/factorial/MainActivity.kt
@@ -2,8 +2,14 @@ package com.svetlana.learn.factorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.svetlana.learn.factorial.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
